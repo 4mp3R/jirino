29371194a84e7a000c65c76e8663e23b491d0cdd
Aleksey Alekseevich Lyulin <codwizard@gmail.com>
2017-12-13 23:32:23 +0100

    Add an awesome ASCII banner and a help message

--- lib/jirino.ex
+++ lib/jirino.ex
@@ -1,5 +1,18 @@
 defmodule Jirino do
   
+  @help_message """
+     __   _        _               
+     \\ \\ (_) _ __ (_) _ __    ___  
+      \\ \\| || '__|| || '_ \\  / _ \\ 
+   /\\_/ /| || |   | || | | || (_) |
+   \\___/ |_||_|   |_||_| |_| \\___/ 
+
+   Jirino by codwizard[at]gmail[dot]com
+
+   Usage:
+   "issue ISSUE_KEY" - shows a summary for an issue
+  """
+
   def main(args) do
     case args do
       ["issue", key] ->
@@ -7,6 +20,8 @@ defmodule Jirino do
         |> Jirino.Api.RemoteCalls.get_issue
         |> Jirino.Api.Issue.format
         |> IO.puts
+      _ ->
+        IO.puts @help_message
     end 
   end
 
