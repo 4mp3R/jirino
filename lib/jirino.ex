efaf281b988fd6985021efb3f87cf374e8660567
Aleksey Alekseevich Lyulin <codwizard@gmail.com>
2017-12-25 05:22:36 +0100

    Add the ability to display the team composition

--- lib/jirino.ex
+++ lib/jirino.ex
@@ -1,28 +1,47 @@
 defmodule Jirino do
-  
+
   @help_message """
-     __   _        _               
-     \\ \\ (_) _ __ (_) _ __    ___  
-      \\ \\| || '__|| || '_ \\  / _ \\ 
+     __   _        _
+     \\ \\ (_) _ __ (_) _ __    ___
+      \\ \\| || '__|| || '_ \\  / _ \\
    /\\_/ /| || |   | || | | || (_) |
-   \\___/ |_||_|   |_||_| |_| \\___/ 
+   \\___/ |_||_|   |_||_| |_| \\___/
 
    Jirino by codwizard[at]gmail[dot]com
 
    Usage:
+   "team" - show the current team composition
    "issue ISSUE_KEY" - shows a summary for an issue
   """
 
   def main(args) do
     case args do
-      ["issue", key] ->
-        key
-        |> Jirino.Api.RemoteCalls.get_issue
-        |> Jirino.Api.Issue.format
+      ["team"] -> show_team()
+      ["issue", key] -> show_issue(key)
+      _ -> show_help_message()
+    end
+  end
+
+  defp show_team do
+    case Application.get_env(:jirino, :team) do
+      empty when empty in [[], nil] ->
+        IO.puts "The team has not been defined yet!"
+      team ->
+        team
+        |> Enum.map(fn(teammate) -> "-> #{teammate}\n" end)
         |> IO.puts
-      _ ->
-        IO.puts @help_message
-    end 
+    end
+  end
+
+  defp show_issue(key) do
+    key
+    |> Jirino.Api.RemoteCalls.get_issue
+    |> Jirino.Api.Issue.format
+    |> IO.puts
+  end
+
+  defp show_help_message do
+    IO.puts @help_message
   end
 
 end
