97aa56952aac41ef0d6d9b99d7ee8caf0a9ae23a
Aleksey Alekseevich Lyulin <codwizard@gmail.com>
2018-02-03 12:14:42 +0100

    Add more tests for Jirino.Issue

--- lib/issue.ex
+++ lib/issue.ex
@@ -15,15 +15,6 @@ defmodule Jirino.Issue do
     created: nil,
     assignee: nil
 
-  def format(issue) do
-    %Jirino.Issue{description: description} = issue
-
-    case description do
-      nil -> format_summary(issue)
-      _ -> format_summary_and_description(issue)
-    end
-  end
-
   @doc """
     Formats a given struct in a short human-readable form.
 
@@ -35,6 +26,11 @@ defmodule Jirino.Issue do
       ...> issue = %Jirino.Issue{ key: "KK-123", type: "Bug", status: "In Code Review", summary: "Summary here", created: date, assignee: "bob@mail.com" }
       ...> Jirino.Issue.format_short issue
       "KK-123 (Bug) - In Code Review :: 2020/02/29 22:11 :: bob@mail.com :: Summary here"
+
+      iex> date = %DateTime{ year: 2020, month: 2, day: 29, zone_abbr: "AMT", hour: 22, minute: 11, second: 7, microsecond: {0, 0}, utc_offset: -14400, std_offset: 0, time_zone: "America/Manaus" }
+      ...> issue = %Jirino.Issue{ key: "KK-123", type: "Bug", status: "In Code Review", summary: "Summary here", created: date, assignee: nil }
+      ...> Jirino.Issue.format_short issue
+      "KK-123 (Bug) - In Code Review :: 2020/02/29 22:11 :: Summary here"
   """
   def format_short(issue) do
     %Jirino.Issue{
@@ -56,6 +52,32 @@ defmodule Jirino.Issue do
       "#{key} (#{type}) - #{status} :: #{formatted_date}#{assignee_string} :: #{summary}"
   end
 
+  @doc """
+    Formats a given struct in a human-readable form.
+
+    ##Params
+      - issue: Jirino.Issue the issues struct that we want to format.
+
+    ##Examples
+      iex> date = %DateTime{ year: 2020, month: 2, day: 29, zone_abbr: "AMT", hour: 22, minute: 11, second: 7, microsecond: {0, 0}, utc_offset: -14400, std_offset: 0, time_zone: "America/Manaus" }
+      ...> issue = %Jirino.Issue{ key: "KK-123", type: "Bug", status: "In Code Review", summary: "Summary here", created: date, assignee: "bob@mail.com", priority: "Critical", creator: "sam@mail.com" }
+      ...> Jirino.Issue.format issue
+      "KK-123 (Bug) - In Code Review :: 2020/02/29 22:11 :: bob@mail.com :: Summary here\\nFather: sam@mail.com, P/Critical"
+
+      iex> date = %DateTime{ year: 2020, month: 2, day: 29, zone_abbr: "AMT", hour: 22, minute: 11, second: 7, microsecond: {0, 0}, utc_offset: -14400, std_offset: 0, time_zone: "America/Manaus" }
+      ...> issue = %Jirino.Issue{ key: "KK-123", type: "Bug", status: "In Code Review", summary: "Summary here", created: date, assignee: "bob@mail.com", priority: "Critical", creator: "sam@mail.com", description: "Description here" }
+      ...> Jirino.Issue.format issue
+      "KK-123 (Bug) - In Code Review :: 2020/02/29 22:11 :: bob@mail.com :: Summary here\\nFather: sam@mail.com, P/Critical\\n===[Description]===\\nDescription here\\n"
+  """
+  def format(issue) do
+    %Jirino.Issue{description: description} = issue
+
+    case description do
+      nil -> format_summary(issue)
+      _ -> format_summary_and_description(issue)
+    end
+  end
+
   defp format_summary(issue) do
     %Jirino.Issue{
       creator: creator,
@@ -69,7 +91,7 @@ defmodule Jirino.Issue do
     %Jirino.Issue{description: description} = issue
 
     format_summary(issue) <> """
-    ===[Description]===
+    \n===[Description]===
     #{description}
     """
   end
