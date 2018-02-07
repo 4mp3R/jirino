0dcdbc0795f120c0c063b011381fefd3508be8f7
Aleksey Alekseevich Lyulin <codwizard@gmail.com>
2018-02-07 22:26:45 +0100

    Add unit test for showing current user's issues

--- test/jirino_test.exs
+++ test/jirino_test.exs
@@ -10,16 +10,15 @@ defmodule JirinoTest do
       {
         Jirino.Utilities,
         [],
-        [get_config: fn(_) -> ["a","b"] end]
+        [get_config: fn key when key == :team -> ["a","b"] end]
       },
       {
         IO,
         [],
-        [puts: fn(_) -> 0 end]
+        [puts: fn(_) -> nil end]
       }
     ]) do
       Jirino.main ["team"]
-      assert called Jirino.Utilities.get_config :team
       assert called IO.puts "-> a\n-> b"
     end
   end
@@ -29,17 +28,50 @@ defmodule JirinoTest do
       {
         Jirino.Utilities,
         [],
-        [get_config: fn(_) -> [] end]
+        [get_config: fn key when key == :team -> [] end]
       },
       {
         IO,
         [],
-        [puts: fn(_) -> 0 end]
+        [puts: fn(_) -> nil end]
       }
     ]) do
       Jirino.main ["team"]
-      assert called Jirino.Utilities.get_config :team
       assert called IO.puts "The team has not been defined yet!"
+    end
+  end
+
+  test "should show user's issues" do
+    issue1 = %Jirino.Issue{ description: "Issue 1" }
+    issue2 = %Jirino.Issue{ description: "Issue 2" }
+
+    with_mocks([
+      {
+        Jirino.Utilities,
+        [],
+        [get_config: fn key when key == :username -> "me@mail.com" end]
+      },
+      {
+        Jirino.RemoteCalls,
+        [],
+        [get_issues_for_users: fn users when users == ["me@mail.com"] -> [issue1, issue2] end]
+      },
+      {
+        Jirino.Issue,
+        [],
+        [format: fn
+          issue when issue == issue1 -> "My formatted issue 1"
+          issue when issue == issue2 -> "My formatted issue 2"
+        end]
+      },
+      {
+        IO,
+        [],
+        [puts: fn(_) -> nil end]
+      }
+    ]) do
+      Jirino.main ["issues"]
+      assert called IO.puts "My formatted issue 1\nMy formatted issue 2"
     end
   end
 end
